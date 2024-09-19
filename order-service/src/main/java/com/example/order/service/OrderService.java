f2e7f3dfeb01e07afd49e7ac14cee6a892dadaec
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-09-19 15:22:22 -0400

    Feign Client

--- order-service/src/main/java/com/example/order/service/OrderService.java
+++ order-service/src/main/java/com/example/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.order.service;
 
+import com.example.order.client.InventoryClient;
 import com.example.order.dto.OrderRequest;
 import com.example.order.dto.OrderResponse;
 import com.example.order.model.Order;
@@ -15,16 +16,26 @@ import java.util.UUID;
 @Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public OrderResponse placeOrder(OrderRequest orderRequest){
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSku(orderRequest.sku());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
-        log.info("Order Created");
-        return new OrderResponse(order.getId(),order.getOrderNumber(),order.getSku(),order.getPrice(),order.getQuantity());
+
+        var inStock = inventoryClient.isInStock(orderRequest.sku(), orderRequest.quantity());
+
+        if (inStock)
+        {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSku(orderRequest.sku());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+            log.info("Order Created");
+            return new OrderResponse(order.getId(), order.getOrderNumber(),
+                    order.getSku(), order.getPrice(), order.getQuantity());
+        } else {
+            throw new RuntimeException(orderRequest.sku()+"not in stock");
+        }
 
     }
 }
