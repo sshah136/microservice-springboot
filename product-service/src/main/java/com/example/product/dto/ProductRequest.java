ef96a6c2304e4ae87505308d77972be2fd718f7e
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-09-19 14:02:40 -0400

    Revert "Initial Commit"
    This reverts commit 4b31dfccd21016b45f950eb8c89810dab5c94a9f.

--- product-service/src/main/java/com/example/product/dto/ProductRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.product.dto;
-
-import java.math.BigDecimal;
-
-public record ProductRequest(String id,
-                             String name,
-                             String description,
-                             BigDecimal price) {
-}
--- product-service/src/main/java/com/example/product/dto/ProductResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.product.dto;
-
-import java.math.BigDecimal;
-
-public record ProductResponse(String id,
-                              String name,
-                              String description,
-                              BigDecimal price) {
-}
