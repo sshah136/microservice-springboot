b6fa0d7b7fa68d3cac621c9f8f771dc4c2fe16e7
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-09-19 16:31:00 -0400

    Integration Test

--- order-service/src/test/java/com/example/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/example/order/OrderServiceApplicationTests.java
@@ -1,15 +1,60 @@
 package com.example.order;
 
+import com.example.order.stubs.InventoryClientStub;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.testcontainers.containers.MySQLContainer;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
 	}
 
+	static {
+		mySQLContainer.start();
+	}
+
+	@Test
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
+                {
+                     "sku": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1
+                }
+                """;
+
+		InventoryClientStub.stubInventoryCall("iphone_15",1);
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order/create")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("orderNumber", Matchers.notNullValue())
+				.body("sku", Matchers.notNullValue())
+				.body("price", Matchers.notNullValue())
+				.body("quantity", Matchers.notNullValue());
+
+	}
 }
