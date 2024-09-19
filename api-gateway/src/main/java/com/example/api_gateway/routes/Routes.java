c64404ea3256afc973a48043c97c0380a77dfd9f
Sahil Shah <91505561+sshah136@users.noreply.github.com>
2024-09-19 13:58:06 -0400

    product service route

---
+++ api-gateway/src/main/java/com/example/api_gateway/routes/Routes.java
@@ -0,0 +1,25 @@
+package com.example.api_gateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.HandlerFunction;
+import org.springframework.web.servlet.function.RequestPredicate;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute(){
+        return GatewayRouterFunctions.route("product_service")
+                .route(RequestPredicates.path("/api/product/products"),
+        HandlerFunctions.http("http://localhost:8080"))
+                .build();
+    }
+
+}
